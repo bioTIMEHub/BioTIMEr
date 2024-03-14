e0c114f3304c82ae04f1ea22761254129164348f
AlbanSagouis <alban.sagouis@idiv.de>
2024-03-14 10:21:22 +0100

    adding Laura as ctb and rev+documentation update

--- R/BioTIMEr-package.R
+++ R/BioTIMEr-package.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 #' @description
-#' The BioTIMEr package is developed at GitHub
+#' The BioTIMEr package is developed in GitHub
 #' (https://github.com/bioTIMEHub/BioTIMEr). To see the preferable citation of
 #' the package, type citation("BioTIMEr"). The associated vignette includes an introduction
 #' to the functions, how to apply them as well as some hints on how to
