894832aac291ab00d76925acde7524a657b48b8d
AlbanSagouis <25483578+AlbanSagouis@users.noreply.github.com>
2024-04-01 16:07:34 +0000

    CRAN submission preparation

--- .devcontainer/install.R
+++ .devcontainer/install.R
@@ -1,2 +1,3 @@
 install.packages("dplyr","tidyr","ggplot2","maps","vegan","dggridR")
-install.packages("formatR","spelling","codemetar")
+install.packages("formatR","spelling","codemetar","pdflatex","qpdf")
+tools::resaveRdaFiles(path = "./data", compression_level = 9)
