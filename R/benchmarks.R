c4c3e055fb7c8b91469a74526cb58d932445b13a
AlbanSagouis <alban.sagouis@idiv.de>
2026-02-09 14:52:11 +0100

    new function parsing SAMPLE_DESC
    Parsing SAMPLE_DESC following structure in SAMPLE_DESC_NAME with documentation, examples and tests. Checks passed locally, win builder and Mac builder

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -572,4 +572,278 @@ if (FALSE) {
   # 1 ref         324.9ms  410.1ms      2.44    9.38MB     6.10     2     5      820ms
   # 2 wide        293.8ms  296.2ms      3.38    8.32MB     8.44     2     5      592ms
   # 3 long         51.3ms   52.9ms     18.6     1.91MB     7.43    10     4      538ms
+
+  # Benchmarking the function parsing SAMPLE_DESC based on _SAMPLE_DESC_NAME
+  v1dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> as.data.frame())
+    for (i in seq_len(NROW(dt))) {
+      colnames(codes[[i]]) <- names[[i]]
+    }
+
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+  v1dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> as.data.frame())
+    for (i in seq_len(NROW(dt))) {
+      colnames(codes[[i]]) <- names[[i]]
+    }
+
+    dplyr::bind_rows(codes)
+  }
+
+  v2dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    for (i in seq_along(codes)) {
+      names(codes[[i]]) = names[[i]]
+      codes[[i]] <- codes[[i]] |> t() |> data.frame()
+    }
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+  v2dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    for (i in seq_along(codes)) {
+      names(codes[[i]]) = names[[i]]
+    }
+    dplyr::bind_rows(codes)
+  }
+  v3dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> as.data.frame())
+    for (i in seq_along(dt)) {
+      colnames(codes[[i]]) <- names[[i]]
+    }
+
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+  v3dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> as.data.frame())
+    for (i in seq_along(dt)) {
+      colnames(codes[[i]]) <- names[[i]]
+    }
+
+    dplyr::bind_rows(codes)
+  }
+
+  v4dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+    lapply(seq_along(codes), function(i) {
+      codes[[i]] |> t() |> as.data.frame() |> stats::setNames(nm = names[[i]])
+    }) |>
+      data.table::rbindlist(fill = TRUE)
+  }
+  v4dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    lapply(seq_along(codes), function(i) {
+      codes[[i]] |> t() |> as.data.frame() |> stats::setNames(nm = names[[i]])
+    }) |>
+      dplyr::bind_rows()
+  }
+
+  v5dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> data.table::as.data.table())
+    lapply(seq_along(codes), function(i) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    })
+
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+  v5dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> data.table::as.data.table())
+    lapply(seq_along(codes), function(i) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    })
+
+    dplyr::bind_rows(codes)
+  }
+
+  v6dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> data.table::as.data.table())
+    for (i in seq_along(codes)) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    }
+
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+  v6dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) x |> t() |> data.table::as.data.table())
+    for (i in seq_along(codes)) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    }
+
+    dplyr::bind_rows(codes)
+  }
+
+  v7dt <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) as.data.frame(matrix(x, nrow = 1)))
+    for (i in seq_along(codes)) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    }
+
+    data.table::rbindlist(codes, fill = TRUE)
+  }
+
+  v7dp <- function(code, name) {
+    codes <- stringi::stri_split_fixed(code, pattern = "_", simplify = FALSE)
+    names <- stringi::stri_split_fixed(name, pattern = "_", simplify = FALSE)
+
+    codes <- lapply(codes, function(x) as.data.frame(matrix(x, nrow = 1)))
+    for (i in seq_along(codes)) {
+      data.table::setnames(codes[[i]], new = names[[i]])
+    }
+
+    dplyr::bind_rows(codes)
+  }
+
+  # Benchmarking
+  ## Conversion
+  codes <- stringi::stri_split_fixed(dt$code, pattern = "_", simplify = FALSE)
+  bench::mark(
+    lapply(codes, function(x) as.data.frame(matrix(x, nrow = 1))),
+    lapply(codes, function(x) x |> t() |> data.table::as.data.table()),
+    lapply(seq_along(codes), function(i) {
+      codes[[i]] |> t() |> as.data.frame() |> stats::setNames(nm = names[[i]])
+    })
+  )
+  ## Total
+  res <- lapply(2:5, function(i) {
+    times <- 10^i
+    dt <- data.frame(
+      code = rep(
+        c(
+          "T1_23.1212_56.2346_1995_12_30",
+          "786_-60.4567_0.346_2027"
+        ),
+        each = times
+      ),
+      name = rep(
+        c("transect_lon_lat_year_month_day", "depth_lat_lon_year"),
+        each = times
+      )
+    )
+
+    bench::mark(
+      check = FALSE,
+      v1dp = v1dp(dt$code, dt$name),
+      v1dt = v1dt(dt$code, dt$name),
+      v2dp = v2dp(dt$code, dt$name),
+      v2dt = v2dt(dt$code, dt$name),
+      v3dp = v3dp(dt$code, dt$name),
+      v3dt = v3dt(dt$code, dt$name),
+      v4dp = v4dp(dt$code, dt$name),
+      v4dt = v4dt(dt$code, dt$name),
+      v5dt = v5dt(dt$code, dt$name),
+      v5dp = v5dp(dt$code, dt$name),
+      v6dt = v6dt(dt$code, dt$name),
+      v6dp = v6dp(dt$code, dt$name),
+      v7dt = v7dt(dt$code, dt$name),
+      v7dp = v7dp(dt$code, dt$name)
+    )
+  }) |>
+    dplyr::bind_rows(.id = "times") |>
+    dplyr::mutate(
+      # times = as.ordered(10^times),
+      expression = as.factor(as.character(expression)),
+      score = as.numeric(median) * as.numeric(mem_alloc)
+    )
+  res <- res |>
+    dplyr::arrange(score, .by = times)
+
+  res |>
+    dplyr::summarise(
+      score = mean(score),
+      median = mean(median),
+      mem_alloc = mean(mem_alloc),
+      .by = expression
+    ) |>
+    dplyr::arrange(score)
+
+  library(ggplot2)
+  res |>
+    dplyr::filter(!grepl("v5|v6", expression, perl = TRUE)) |>
+    ggplot(aes(x = times, y = score, group = expression)) +
+    scale_y_log10() +
+    geom_line(aes(col = expression, shape = expression))
+
+  #   times  expression        min   median `itr/sec` mem_alloc
+  #  1 1     v4dt(dt$cod…   2.18ms   2.21ms  445.       40.27KB
+  #  2 1     v2dp(dt$cod…   1.76ms   1.83ms  537.       72.94KB
+  #  3 1     v4dp(dt$cod…   3.09ms   3.16ms  310.       43.41KB
+  #  4 1     v3dt(dt$cod…   2.02ms   2.08ms  471.       95.84KB
+  #  5 1     v1dt(dt$cod…   2.03ms   2.11ms  462.       104.1KB
+  #  6 1     v3dp(dt$cod…   3.33ms   3.46ms  285.      130.29KB
+  #  7 1     v1dp(dt$cod…    3.3ms    3.5ms  284.      129.04KB
+  #  8 1     v2dt(dt$cod…   5.99ms   6.32ms  158.       83.83KB
+  #  9 2     v4dt(dt$cod…  22.25ms  22.99ms   43.6      251.2KB
+  # 10 2     v2dp(dt$cod…  16.61ms  16.98ms   58.7     400.39KB
+  # 11 2     v3dt(dt$cod…  20.04ms  20.29ms   48.9      368.7KB
+  # 12 2     v1dt(dt$cod…  20.13ms  20.45ms   48.9      368.7KB
+  # 13 2     v4dp(dt$cod…  32.35ms  33.04ms   30.2     416.06KB
+  # 14 2     v2dt(dt$cod…  60.52ms  60.73ms   16.4     235.53KB
+  # 15 1     v5dt(dt$cod…   8.06ms   8.34ms  119.         3.2MB
+  # 16 1     v6dt(dt$cod…   8.23ms   8.58ms  116.        3.24MB
+  # 17 2     v1dp(dt$cod…  33.37ms  34.43ms   29.0     850.02KB
+  # 18 2     v3dp(dt$cod…  33.93ms   35.4ms   28.3     853.26KB
+  # 19 1     v6dp(dt$cod…  25.43ms  25.82ms   38.7       9.55MB
+  # 20 1     v5dp(dt$cod…  25.25ms  27.23ms   35.0       9.52MB
+  # 21 3     v4dt(dt$cod… 418.02ms 423.18ms    2.36      2.31MB
+  # 22 3     v2dp(dt$cod…  220.4ms 255.22ms    3.92      3.89MB
+  # 23 3     v1dt(dt$cod… 255.21ms 298.25ms    3.35      3.45MB
+  # 24 3     v3dt(dt$cod… 281.29ms 304.68ms    3.28      3.45MB
+  # 25 3     v2dt(dt$cod… 893.54ms 893.54ms    1.12      2.15MB
+  # 26 2     v6dt(dt$cod…  84.89ms  85.37ms   11.6      31.83MB
+  # 27 2     v5dt(dt$cod…  84.23ms  87.12ms   11.4      31.85MB
+  # 28 3     v4dp(dt$cod…  855.9ms  855.9ms    1.17      4.05MB
+  # 29 3     v3dp(dt$cod… 507.71ms 507.71ms    1.97      8.32MB
+  # 30 3     v1dp(dt$cod… 540.89ms 540.89ms    1.85      8.31MB
+  # 31 2     v6dp(dt$cod… 265.17ms 265.17ms    3.77     95.25MB
+  # 32 2     v5dp(dt$cod… 277.68ms 277.68ms    3.60     95.27MB
+  # 33 4     v1dt(dt$cod…    3.07s    3.07s    0.325    34.35MB
+  # 34 4     v4dt(dt$cod…    4.12s    4.12s    0.243    27.99MB
+  # 35 4     v3dt(dt$cod…    4.42s    4.42s    0.226    34.35MB
+  # 36 4     v4dp(dt$cod…    3.96s    3.96s    0.252    40.44MB
+  # 37 4     v2dt(dt$cod…    7.51s    7.51s    0.133    21.38MB
+  # 38 4     v2dp(dt$cod…    4.48s    4.48s    0.223    38.91MB
+  # 39 3     v5dt(dt$cod…    1.27s    1.27s    0.786   318.31MB
+  # 40 3     v6dt(dt$cod…    1.37s    1.37s    0.728   318.16MB
+  # 41 4     v1dp(dt$cod…    7.08s    7.08s    0.141    83.16MB
+  # 42 4     v3dp(dt$cod…    7.92s    7.92s    0.126    83.16MB
+  # 43 3     v5dp(dt$cod…    4.02s    4.02s    0.249   952.82MB
+  # 44 3     v6dp(dt$cod…    4.52s    4.52s    0.221   952.67MB
+  # 45 4     v6dt(dt$cod…   13.87s   13.87s    0.0721    3.11GB
+  # 46 4     v5dt(dt$cod…   16.43s   16.43s    0.0609    3.11GB
+  # 47 4     v6dp(dt$cod…   33.24s   33.24s    0.0301     9.3GB
+  # 48 4     v5dp(dt$cod…   35.25s   35.25s    0.0284     9.3GB
 }
